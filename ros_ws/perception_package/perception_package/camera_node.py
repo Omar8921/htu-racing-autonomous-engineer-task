f8c021c333f752af1b07da4986406954657d023b
Omar <omaralmadani05@gmail.com>
2026-08-12 12:30:37 -0400

    Make perception package skeleton

---
+++ ros_ws/perception_package/perception_package/camera_node.py
@@ -0,0 +1,7 @@
+from rclpy.node import Node
+
+class CameraNode(Node):
+    pass
+
+if __name__ == '__main__':
+    pass
---
+++ ros_ws/perception_package/perception_package/lidar_node.py
@@ -0,0 +1,7 @@
+from rclpy.node import Node
+
+class LidarNode(Node):
+    pass
+
+if __name__ == '__main__':
+    pass
